37980f6f4e5c847634162d684c68472fcdc08803
jiseon723 <dighxxx12@gmail.com>
2025-10-16 09:14:05 +0900

    init

---
+++ src/test/java/com/_2/proj_02/Proj02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com._2.proj_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proj02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
